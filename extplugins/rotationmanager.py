35ea4ed5b7829d423fb6b9e191d79eb48ec8a63b
Just a baka <justabaka@gmail.com>
2010-09-27 05:51:16 +0700

    Gametype history is now really disabled by default.

--- extplugins/rotationmanager.py
+++ extplugins/rotationmanager.py
@@ -70,7 +70,7 @@ class RotationmanagerPlugin(b3.plugin.Plugin):
   _recentmaps = []                      # The Maphistory
   _recentgts = []                       # The Gametype history
   _hmm = 4                              # HowManyMaps to keep as a maphistory
-  _hmgt = 2                             # HowManyGameTypes to keep as a gametype history
+  _hmgt = 0                             # HowManyGameTypes to keep as a gametype history
 
   def onStartup(self):
     """\
